dc05183aa610c51adb4c8ce42b1d645ec52b7bc5
uedazxcvbnm <kd1325559@st.kobedenshi.ac.jp>
2024-04-24 15:36:19 +0900

    comment0424

---
+++ comment/comment_detail.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+    <head> 
+        <link rel="stylesheet" href="comment.css">
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <!-- 質問 -->
+            <div class="detail1">
+                <div class="detail_q">
+                    <h3 style="text-align:left; float:left;">質問</h3>
+                    <div style="text-align:right; padding-top:20px;">12回答</div>
+                    <br>
+                    <table>
+                        <tr>
+                            <td><img class="user_icon" src="./image/user-icon1.png" width="40px" height="40px"><div class="user_name">&nbsp;morimoriさん</div></td>
+                        </tr>
+                        <tr>
+                            <td>〇〇はどのようにすればいいですか？</td>
+                        </tr>
+                    </table>
+                </div>
+                <!-- コメント欄 -->
+                <!-- 質問者の返信がある場合 -->
+                <div class="detail_reply">
+                    <h3>回答</h3>
+                    <table>
+                        <tr>
+                            <td>このようにすればできる</td>
+                        </tr>
+                    </table>
+                </div>
+                <!-- コメント記入 -->
+                <!-- <br> -->
+                <div class="write_comment">
+                    <!-- <label>コメント記入</label> -->
+                    <p><textarea rows="3" cols="45" placeholder="質問に回答する"></textarea></p>
+                    <p><button>送信</button></p>
+                </div>
+            </div>
+    </body>
+</html>
+
+
+<!-- <tr align="left">
+                <td id="a"></td>
+            </tr>
+            <tr align="right">
+                <td>返信</td>
+            </tr> -->
