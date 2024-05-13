5cbc46da25899f54c2ba583c07a0454176a93193
ShunsakuKatsumata <kd1310700@st.kobedenshi.ac.jp>
2024-05-13 11:43:28 +0900

    アカウントごとの編集削除

--- comment/post_answer/answer_post_delete.php
+++ comment/post_answer/answer_post_delete.php
@@ -1,24 +1,13 @@
 <?php
-session_start();
-
-// ログイン中のユーザーIDを取得
-$user_id = $_SESSION['user']['user_id'];
-
 // 変数を受け取る
 $comment_id = $_GET['commentId'];
 $post_id = $_GET['post_id'];
 
-// 回答の所有者を取得
+// 回答の削除
 require_once __DIR__ . '/../classes/answer_post.php';
 $answer_post = new answer_post();
-$owner_id = $answer_post->getAnswerOwner($comment_id);
+$answer_post->delete_answer($comment_id);
 
-// ユーザーが回答の所有者かどうかを確認
-if ($user_id == $owner_id) {
-    // 回答の削除
-    $answer_post->delete_answer($comment_id);
-}
-
-// comment_detail画面に移動
+// comment_home画面に移動
 header("Location: ./../comment_detail.php?ident=$post_id");
 exit;
